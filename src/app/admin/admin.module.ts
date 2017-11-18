4f7e7cd6c6e92cd23627c6f970d9a3bf6a31e16d
USER <kwlsy666@naver.com>
2017-11-18 17:48:48 +0900

    admin 상단 공통 메뉴

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,11 +4,18 @@ import { AdminComponent } from './admin/admin.component';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import {AdminRoutingModule} from "./admin.routing.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule, MatCardModule} from "@angular/material";
 
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   declarations: [AdminComponent, HomeComponent, NewsComponent]
 })
